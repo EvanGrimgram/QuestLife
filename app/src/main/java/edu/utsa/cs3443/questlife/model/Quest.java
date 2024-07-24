f3fd89070381c7c2f1be6c9e6c67707d6b594e9f
vic1702 <danielgarc2002@gmail.com>
2024-07-23 22:23:48 -0500

    Quest.java

--- app/src/main/java/edu/utsa/cs3443/questlife/model/Quest.java
+++ app/src/main/java/edu/utsa/cs3443/questlife/model/Quest.java
@@ -1,4 +1,39 @@
 package edu.utsa.cs3443.questlife.model;
 
 public class Quest {
+    private String userInput;
+    private ArrayList <Quest> userQuests;
+
+    // Constructor
+    public Quest(String userInput) {
+        this.userInput = userInput;
+    }
+
+    // Getter and Setter for User Input
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    // Getter and Setter for userQuests array
+    public ArrayList<Quest> getUserQuests() {
+        return userQuests;
+    }
+
+    public void setUserQuests(ArrayList<Quest> userQuests) {
+        this.userQuests = userQuests;
+    }
+
+    // Method to load quests from array
+    public void loadQuests(ArrayList<Quest> questToLoad) {
+        this.userQuests = questToLoad;
+    }
+
+    // Method to add quests
+    public void addQuest(Quest quest) {
+        this.userQuests.add(quest);
+    }
 }
