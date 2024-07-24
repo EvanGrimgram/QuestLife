80d87e552f2ca7b7791523d9b0e3f7866d726806
evang <evangmasterchief@gmail.com>
2024-07-24 13:48:06 -0500

    New button to submit

--- app/src/main/java/edu/utsa/cs3443/questlife/QuestActivity.java
+++ app/src/main/java/edu/utsa/cs3443/questlife/QuestActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import com.google.android.material.textfield.TextInputEditText;
 public class QuestActivity extends AppCompatActivity {
 
     private Button returnButton;
+    private Button submitButton;
     private TextInputEditText inputText;
 
     @Override
@@ -25,6 +27,7 @@ public class QuestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quest);
 
         returnButton = findViewById(R.id.returnButton);
+        submitButton = findViewById(R.id.buttonSubmit);
         inputText = findViewById(R.id.InputText);
 
         returnButton.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +36,14 @@ public class QuestActivity extends AppCompatActivity {
                 // Create an Intent to start the new activity
                 Intent intent = new Intent(QuestActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the new activity
+                Toast.makeText(QuestActivity.this, "Submitted!", Toast.LENGTH_SHORT).show();
             }
         });
 
