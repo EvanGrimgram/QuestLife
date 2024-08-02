01842ee1787773ae4a77ee15fb0ed79a1974d335
vic1702 <danielgarc2002@gmail.com>
2024-08-01 19:09:52 -0500

    Updated HistoryActivity to display new centered and bigger image view of defeated enemies

--- app/src/main/java/edu/utsa/cs3443/questlife/HistoryActivity.java
+++ app/src/main/java/edu/utsa/cs3443/questlife/HistoryActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import android.content.Intent;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,12 +16,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 import edu.utsa.cs3443.questlife.model.BossHistory;
+import edu.utsa.cs3443.questlife.model.Quest;
+import edu.utsa.cs3443.questlife.model.UserQuests;
 
 public class HistoryActivity extends AppCompatActivity {
 
     private Button returnButton;
     private LinearLayout enemyHistoryLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,7 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
         returnButton = findViewById(R.id.returnButton);
-        enemyHistoryLayout = findViewById(R.id.enemyHistoryLayout);
+        enemyHistoryLayout = findViewById(R.id.enemyContainer);
 
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,12 +61,27 @@ public class HistoryActivity extends AppCompatActivity {
 
     private void displayBossHistory() {
         enemyHistoryLayout.removeAllViews();
-        for (BossHistory.DefeatedEnemy enemy : BossHistory.getInstance().getDefeatedEnemies()) {
-            addDefeatedEnemy(enemy);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        List<BossHistory.DefeatedEnemy> defeatedEnemies = BossHistory.getInstance().getDefeatedEnemies();
+
+        for (BossHistory.DefeatedEnemy enemy : defeatedEnemies) {
+            View cardView = inflater.inflate(R.layout.defeated_enemy_card, enemyHistoryLayout, false);
+
+            ImageView enemyImageView = cardView.findViewById(R.id.enemyImageView);
+            TextView enemyNameTextView = cardView.findViewById(R.id.textEnemy);
+            TextView enemyHealthTextView = cardView.findViewById(R.id.textEnemyOriginalHealth);
+
+            enemyImageView.setImageResource(enemy.getImageResource());
+            enemyNameTextView.setText(enemy.getName());
+            enemyHealthTextView.setText(String.valueOf(enemy.getOriginalHealth()));
+
+            enemyHistoryLayout.addView(cardView);
         }
     }
 
-    private void addDefeatedEnemy(BossHistory.DefeatedEnemy enemy) {
+
+
+    /*private void addDefeatedEnemy(BossHistory.DefeatedEnemy enemy) {
         LinearLayout enemyLayout = new LinearLayout(this);
         enemyLayout.setOrientation(LinearLayout.HORIZONTAL);
         enemyLayout.setGravity(Gravity.CENTER_VERTICAL);
@@ -79,5 +100,5 @@ public class HistoryActivity extends AppCompatActivity {
         enemyLayout.addView(enemyTextView);
 
         enemyHistoryLayout.addView(enemyLayout);
-    }
+    }*/
 }
